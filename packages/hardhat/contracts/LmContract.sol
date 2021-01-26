4c3c0e44be61be5caa2fecf14832736a9f77428d
singyiu <singyiu@gmail.com>
2021-01-25 21:58:40 -0800

    collectYield

--- packages/hardhat/contracts/LmContract.sol
+++ packages/hardhat/contracts/LmContract.sol
@@ -17,7 +17,7 @@ contract LmContract {
         string description;
         string imageUrl;
         string linkUrl;
-        uint stakePrice; //stakePrice of membership cannot be changed ?
+        uint stakePrice;
         uint numOfMembers; //only store the number of members, this contract won't store a list of all members subscribed
         bool isActive;
     }
@@ -27,28 +27,40 @@ contract LmContract {
         address beneficiary;
         uint stakeSum;
         uint createMStakePrice;
+        uint yieldSum;
+    }
+
+    struct UserJoinDetail {
+        uint joinAt;
+        uint stakePrice;
     }
 
     mapping(uint => Membership) public mpMap; //map (membership.id) => (Membership program) for storing all membership programs
     mapping(uint => MembershipDetail) private mpDetailMap; //map (membership.id) => (Membership program private detail)
     mapping(address => uint[]) private mpListMap; //map (owner.address) => (array of membership.id created) for storing a list of membership program for each service provider, private
 
-    mapping(address => mapping(uint => uint)) private umMap; //map (user.address)(membership.id) => (stake price at the time of joining the membership)
-    mapping(address => uint[]) private umListMap; //map (user.address) => (array of membership.id joined) for storing a list of memberships for each user, private
+    mapping(address => mapping(uint => UserJoinDetail)) private ujMap; //map (user.address)(membership.id) => (UserJoinDetail)
+    mapping(address => uint[]) private ujListMap; //map (user.address) => (array of membership.id joined) for storing a list of memberships for each user, private
 
     uint public createMStakePrice = 1e18; //amount a service provider need to stake to create one membership program
     uint public numOfMP = 0; //membership programs counter
     uint public numOfAllMembers = 0; //member counter
+
     uint private allMembershipStakeSum = 0;
     uint private allCreateMStakeSum = 0;
+    uint public allYieldSum = 0;
+
+    uint public lastYieldCollectedAt = 0;
 
     event MembershipCreateEvent(uint mId, address owner, string title); //a new membership program is created
     event MembershipJoinEvent(uint mId); //user join a membership program
     event MembershipUnsubscribeEvent(uint mId); //user unsubscribe a membership program
     event StakeEvent(uint amount); //deposit to pool
     event UnStakeEvent(uint amount); //withdraw from pool
+    event YieldCollectEvent(uint amount);
 
     constructor() public { //TODO: add owner
+        lastYieldCollectedAt = block.timestamp;
     }
 
     function aDaiBalance() external view returns (uint) {
@@ -70,7 +82,7 @@ contract LmContract {
         //assign membership obj to the membership program map with updated index
         uint mId = ++numOfMP;
         mpMap[mId] = Membership(msg.sender, _title, _description, _imageUrl, _linkUrl, _stakePrice, 0, true);
-        mpDetailMap[mId] = MembershipDetail(msg.sender, _beneficiary, 0, createMStakePrice);
+        mpDetailMap[mId] = MembershipDetail(msg.sender, _beneficiary, 0, createMStakePrice, 0);
         mpListMap[msg.sender].push(mId);
         allCreateMStakeSum += createMStakePrice;
 
@@ -90,14 +102,14 @@ contract LmContract {
     function stakeAndJoinMembership(uint _mId) external {
         Membership memory m = mpMap[_mId];
         require(m.isActive, "Target membership is inactive"); //require target membership to be active
-        require(umMap[msg.sender][_mId] == 0, "Already a member of this membership program"); //require user had not joined the target membership yet
+        require(ujMap[msg.sender][_mId].joinAt == 0, "Already a member of this membership program"); //require user had not joined the target membership yet
         require(dai.transferFrom(msg.sender, address(this), m.stakePrice), "Transfer DAI to contract failed"); //require the target stake price of this membership to be transfered to this contract
         require(dai.approve(address(pool), m.stakePrice), "approve DAI for stake pool failed"); //require the target amount to be approved for the pool
         pool.deposit(address(dai), m.stakePrice, address(this), 0); //deposit the target amount to pool
 
         //add user to join the target membership
-        umMap[msg.sender][_mId] = m.stakePrice;
-        umListMap[msg.sender].push(_mId);
+        ujMap[msg.sender][_mId] = UserJoinDetail(block.timestamp, m.stakePrice);
+        ujListMap[msg.sender].push(_mId);
         mpMap[_mId].numOfMembers++;
         numOfAllMembers++;
         mpDetailMap[_mId].stakeSum += m.stakePrice;
@@ -108,11 +120,11 @@ contract LmContract {
     }
 
     function withdrawAndUnsubscribeFromMembership(uint _mId) external {
-        uint originalStakePrice = umMap[msg.sender][_mId];
+        uint originalStakePrice = ujMap[msg.sender][_mId].stakePrice;
         require(originalStakePrice > 0, "Not a member of this membership program"); //require user to be a member of the target membership program
-        delete umMap[msg.sender][_mId]; //delete user membership record
+        delete ujMap[msg.sender][_mId]; //delete user membership record
 
-        //mId stays in umListMap as a list of history, where umMap is the source of truth of user membership
+        //mId stays in ujListMap as a list of history, where ujMap is the source of truth of user membership
 
         if (mpDetailMap[_mId].stakeSum >= originalStakePrice) {
             mpDetailMap[_mId].stakeSum -= originalStakePrice;
@@ -137,8 +149,24 @@ contract LmContract {
 
     function collectYield() external { //TODO: owneronly
         uint totalStake = allMembershipStakeSum + allCreateMStakeSum;
-        uint totalYield = aDai.balanceOf(address(this)) - totalStake;
-        //TODO: Calculate the portion of yeild to be transfer to the corresponding beneficiary
+        uint aDaiBalance = aDai.balanceOf(address(this));
+        if (aDaiBalance > totalStake) {
+            uint totalYield = aDai.balanceOf(address(this)) - totalStake;
+            require(aDai.approve(address(pool), totalYield)); //approve the totalYield to be withdraw by the pool
+            //loop through each active membership program, calculate and transfer the yield
+            for(uint i=1; i<=numOfMP; i++) {
+                MembershipDetail memory md = mpDetailMap[i];
+                if (mpMap[i].isActive) {
+                    uint y = totalYield * md.stakeSum / totalStake;
+                    pool.withdraw(address(dai), y, md.beneficiary);
+                    mpDetailMap[i].yieldSum += y;
+                } 
+            }
+            //TODO: collect yeild for contract owner
+            allYieldSum += totalYield;
+            lastYieldCollectedAt = block.timestamp;
+            emit YieldCollectEvent(totalYield);
+        }
     }
 
     function getMyMembershipProgramIdList() external view returns (uint[] memory) {
@@ -146,10 +174,10 @@ contract LmContract {
     }
 
     function getMyUserMembershipIdList() external view returns (uint[] memory) {
-        return umListMap[msg.sender];
+        return ujListMap[msg.sender];
     }
 
     function isUserAMember(address _userAddress, uint _membershipProgramId) external view returns (bool) {
-        return (umMap[_userAddress][_membershipProgramId] > 0);
+        return (ujMap[_userAddress][_membershipProgramId].joinAt > 0);
     }
 }
