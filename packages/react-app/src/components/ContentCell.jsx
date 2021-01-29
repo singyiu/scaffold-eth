2f42ddec3be1bbe291e405c4d50be542310b81bc
singyiu <singyiu@gmail.com>
2021-01-28 22:45:00 -0800

    MpCell

--- packages/react-app/src/components/ContentCell.jsx
+++ packages/react-app/src/components/ContentCell.jsx
@@ -4,23 +4,33 @@ import makeStyles from '@material-ui/styles/makeStyles';
 //import Badge from '@material-ui/core/Badge';
 import Avatar from '@material-ui/core/Avatar';
 import 'semantic-ui-css/semantic.min.css';
-import { Grid, Container, Header, Button, Icon, Popup, Modal, Image, Form } from 'semantic-ui-react';
+import { Grid, Container, Header, Button, Icon, Popup, Modal, Image, Form, Card, Label } from 'semantic-ui-react';
 import { ethers } from "ethers";
 import { abi as IErc20 } from '../views/abis/erc20.json'
 import { parseUnits, formatUnits } from "@ethersproject/units";
 import { notification } from "antd";
 import { useExchangePrice, useGasPrice, useUserProvider, useContractLoader, useContractReader, useEventListener, useBalance, useExternalContractLoader } from "../hooks";
 
+{/*
+root: {
+  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  border: 0,
+  borderRadius: 3,
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  color: 'white',
+  height: 48,
+  padding: '0 30px',
+},
+*/}
+
+function openUrlInNewWindow(url) {
+  const win = window.open(url, '_blank');
+  if (win != null) {
+    win.focus();
+  }
+}
+
 const useStyles = makeStyles({
-    root: {
-      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-      border: 0,
-      borderRadius: 3,
-      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-      color: 'white',
-      height: 48,
-      padding: '0 30px',
-    },
     avatar: {
         margin: 0,
       },
@@ -29,6 +39,12 @@ const useStyles = makeStyles({
         width: 134,
         height: 134,
     },
+    cardPadding: {
+        padding: '30px 15px',
+    },
+    mpCard: {
+        borderRadius: 30,
+    }
 });
 
 const ContentCell = (props) => {
@@ -181,10 +197,107 @@ const AddMPButton = (props) => {
 const MpCell = (props) => {
     const classes = useStyles();
     const mp = useContractReader(props.readContracts,"LmContract", "mpMap", [props.mpId])
+    const [isPicked, setIsPicked] = useState(false)
+    const [isDaiApproved, setIsDaiApproved] = useState(false)
+    const [isDaiApproving, setIsDaiApproving] = useState(false)
+    const [isJoining, setIsJoining] = useState(false)
+
+    const approveDai = async (_amount) => {
+      console.log("approveDai",_amount)
+      try {
+      setIsDaiApproving(true)
+      let tokenContract = new ethers.Contract("0x6B175474E89094C44Da98b954EedeAC495271d0F", IErc20, props.signer);
+      let approval = await tokenContract.approve(props.writeContracts.LmContract.address, _amount)
+      console.log('approval', approval)
+      setIsDaiApproving(false)
+      setIsDaiApproved(true)
+      notification.open({
+        message: 'Token transfer approved',
+        description:
+        `Dropletpool can now move up to ${formatUnits(_amount,18)} DAI`,
+      }) } catch (e) {
+        console.log(e)
+        setIsDaiApproving(false)
+        setIsDaiApproved(false)
+        notification.open({
+          message: 'Approval failed',
+          description:
+          `DAI approval did not take place`,
+        })
+      }
+    }
+ 
+    const stakeAndJoinMembership = async () => {
+        console.log("stakeAndJoinMembership")
+        setIsJoining(true)
+        try {
+            await props.tx( props.writeContracts.LmContract.stakeAndJoinMembership(props.mpId) )
+            notification.open({
+                message: 'stakeAndJoinMembership succeed',
+                description: `Membership program ${mp.title} joined`,
+            })
+            setIsPicked(false)
+        } catch (e) {
+            notification.open({
+                message: 'stakeAndJoinMembership failed',
+                description: `stakeAndJoinMembership did not take place`,
+            })
+        }
+        setIsJoining(false)
+    }
 
     return (
-        <div>
-            <Avatar alt="Avatar" src={mp ? mp.imageUrl : null} className={classes.bigAvatar} onClick={props.playFunc}/>
+        <div className={classes.cardPadding}>
+        {mp ?
+            <Card>
+              <Image src={mp.imageUrl} wrapped ui={false} />
+              <Card.Content>
+                <Card.Header>{mp.title}</Card.Header>
+                <Card.Meta>
+                  <span className='date'>Joined in 2015</span>
+                </Card.Meta>
+                <Card.Description>
+                  {mp.description}
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <Grid columns={2}>
+                <Grid.Column>
+                    <Button as='div' labelPosition='right' onClick={() => openUrlInNewWindow(mp.linkUrl)}>
+                      <Button color='red'><Icon name='user' /></Button>
+                      <Label as='a' basic color='red' pointing='left'>{mp.numOfMembers.toString()}</Label>
+                    </Button>
+                </Grid.Column>
+                <Grid.Column>
+                    <Button as='div' labelPosition='right' onClick={() => setIsPicked(!isPicked)}>
+                      <Button color='green'><Icon name='dollar' /></Button>
+                      <Label as='a' basic color='green' pointing='left'>{formatUnits(mp.stakePrice,18)}</Label>
+                    </Button>
+                </Grid.Column>
+                {isPicked ? 
+                <Grid.Row>
+                <Grid.Column>
+                    <Button color='orange' disabled={isDaiApproved} loading={isDaiApproving} onClick={() => approveDai(mp.stakePrice)}>
+                      {isDaiApproved ? 'Approved' : 'Approve'} {formatUnits(mp.stakePrice,18)} DAI
+                    </Button>
+                </Grid.Column>
+                <Grid.Column>
+                    <Button
+                      content="Join"
+                      labelPosition='right'
+                      icon='checkmark'
+                      positive
+                      disabled={!isDaiApproved}
+                      loading={isJoining}
+                      onClick={() => stakeAndJoinMembership(mp.stakePrice)}
+                    />
+                </Grid.Column>
+                </Grid.Row>
+                : null}
+                </Grid>
+              </Card.Content>
+            </Card>
+        : null }
         </div>
     );
 };
--- packages/react-app/src/views/ServiceProvider.jsx
+++ packages/react-app/src/views/ServiceProvider.jsx
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from "react";
 import { MpCell, AddMPButton } from '../components/ContentCell'
 import { notification } from "antd";
+import { Card } from 'semantic-ui-react';
 
 export default function ServiceProvider({purpose, setPurposeEvents, address, mainnetProvider, userProvider, localProvider, yourLocalBalance, price, tx, readContracts, writeContracts, signer, daiContract, serviceProviderMpIds }) {
 
@@ -30,14 +31,19 @@ export default function ServiceProvider({purpose, setPurposeEvents, address, mai
 
     return (
         <div>
+            <Card.Group centered itemsPerRow={4}>
             {serviceProviderMpIds ? serviceProviderMpIds.map((mpId) => {
               return (
                 <MpCell
                 readContracts={readContracts}
                 mpId={mpId}
+                writeContracts={writeContracts}
+                signer={signer}
+                tx={tx}
                 />
               )
             }) : null}
+            </Card.Group>
             <AddMPButton writeContracts={writeContracts} signer={signer} tx={tx}/>
         </div>
     );
